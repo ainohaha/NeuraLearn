8f5976714694c1fa24bb2ef8fb8a6f3da573cfa2
ainohaha <eyenoaino@gmail.com>
2026-04-03 11:08:13 -0700

    Initial Commit

---
+++ SageOS/SageOSApp.swift
@@ -0,0 +1,33 @@
+//
+//  SageOSApp.swift
+//  SageOS
+//
+//  Created by Aino Halonen on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SageOSApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
